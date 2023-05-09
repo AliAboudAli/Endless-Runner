8b43af8496416854cd6120570a52ca22ab3cbb8d
peppie9728 <56298154+peppie9728@users.noreply.github.com>
2023-05-09 11:45:53 +0200

    world start

---
+++ Assets/Pepijn/MapGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class MapGenerator : MonoBehaviour
+{
+    public Tilemap tileMap;
+    public Tile[] tileArray;
+    public GameObject camera;
+
+    public void Update()
+    {
+        
+    }
+
+
+    public void GenerateModule()
+    {
+
+    }
+
+    public void DestroyModule()
+    {
+
+    }
+}
